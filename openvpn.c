df07beb492764950a76e498ef28f08821a095378
Selva Nair <selva.nair@gmail.com>
2016-02-27 13:10:49 -0500

    Support sending commands to running instance
    - New option --command <action> <params> to send commands to
      a running instance of openvpn-gui.exe
      Supported actions are
          connect, disconnect, reconnect
      each of which takes the name of the config (with or without the
      extension .ovpn) as a parameter;
          disconnect_all, exit
      which take no parameter and
          silent_connection
      which takes an optional parameter = 0 or 1 (1 is the default)
    
      Examples: with the gui running, start a new instance as
    
      openvpn-gui.exe --command disconnect myvpn : ask running instance
                            to disconnect myvpn if connected
      openvpn-gui.exe --command status myvpn     : ask running instance
                            to show the status window for myvpn if available
      openvpn-gui.exe --command disconnect_all   : ask running instance
                            to disconnect all active connections
    
    - The second instance exits after issuing a SendMessage to the
      already running instance. If no action is specified, the running
      instance is notified to show a balloon to alert the user
    
    - These messages may also be sent from scripts as COPYDATA messages
      with the wData element specifying the action to execute and lpData
      a pointer to the parameter. The dwData param must be one of
      WM_OVPN_xxx with xxx = START, STOP, RESTART, STOPALL, EXIT or
      SILENT. See main.h for their values.
    
    v2: Bug fixes based on test reports from larson0815
    here: https://github.com/selvanair/openvpn-gui/issues/5
    and cron410 here: https://github.com/OpenVPN/openvpn-gui/issues/104
    
    Signed-off-by: Selva Nair <selva.nair@gmail.com>

--- openvpn.c
+++ openvpn.c
@@ -49,10 +49,6 @@
 #include "access.h"
 #include "save_pass.h"
 
-#define WM_OVPN_STOP    (WM_APP + 10)
-#define WM_OVPN_SUSPEND (WM_APP + 11)
-#define WM_OVPN_RESTART (WM_APP + 12)
-
 extern options_t o;
 
 static BOOL
@@ -1440,6 +1436,9 @@ StatusDialogFunc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 
     case WM_OVPN_STOP:
         c = (connection_t *) GetProp(hwndDlg, cfgProp);
+        /* external messages can trigger when we are not ready -- check the state */
+        if (!IsWindowEnabled(GetDlgItem(c->hwndStatus, ID_DISCONNECT)))
+            break;
         c->state = disconnecting;
         RunDisconnectScript(c, false);
         EnableWindow(GetDlgItem(c->hwndStatus, ID_DISCONNECT), FALSE);
@@ -1447,7 +1446,7 @@ StatusDialogFunc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam)
         SetMenuStatus(c, disconnecting);
         SetDlgItemText(c->hwndStatus, ID_TXT_STATUS, LoadLocalizedString(IDS_NFO_STATE_WAIT_TERM));
         SetEvent(c->exit_event);
-        SetTimer(hwndDlg, IDT_STOP_TIMER, 3000, NULL);
+        SetTimer(hwndDlg, IDT_STOP_TIMER, 15000, NULL);
         break;
 
     case WM_OVPN_SUSPEND:
@@ -1458,7 +1457,7 @@ StatusDialogFunc(HWND hwndDlg, UINT msg, WPARAM wParam, LPARAM lParam)
         SetMenuStatus(c, disconnecting);
         SetDlgItemText(c->hwndStatus, ID_TXT_STATUS, LoadLocalizedString(IDS_NFO_STATE_WAIT_TERM));
         SetEvent(c->exit_event);
-        SetTimer(hwndDlg, IDT_STOP_TIMER, 3000, NULL);
+        SetTimer(hwndDlg, IDT_STOP_TIMER, 15000, NULL);
         break;
 
     case WM_TIMER:
@@ -1604,12 +1603,19 @@ StartOpenVPN(connection_t *c)
 
     if (c->hwndStatus)
     {
-        PrintDebug(L"Connection request when previous status window is still open -- ignored");
-        WriteStatusLog(c, L"OpenVPN GUI> ",
-                       L"Complete the pending dialog before starting a new connection", false);
-        SetForegroundWindow(c->hwndStatus);
+        PrintDebug(L"Connection request when already started -- ignored");
+        /* the tread can hang around after disconnect if user has not dismissed any popups */
+        if (c->state == disconnected)
+            WriteStatusLog(c, L"OpenVPN GUI> ",
+                       L"Complete any pending dialog before starting a new connection", false);
+        if (!o.silent_connection)
+        {
+           SetForegroundWindow(c->hwndStatus);
+           ShowWindow(c->hwndStatus, SW_SHOW);
+        }
         return FALSE;
     }
+    PrintDebug(L"Starting openvpn on config %s", c->config_name);
 
     RunPreconnectScript(c);
 
@@ -1813,7 +1819,10 @@ SuspendOpenVPN(int config)
 void
 RestartOpenVPN(connection_t *c)
 {
-    PostMessage(c->hwndStatus, WM_OVPN_RESTART, 0, 0);
+    if (c->hwndStatus)
+        PostMessage(c->hwndStatus, WM_OVPN_RESTART, 0, 0);
+    else /* Not started: treat this as a request to connect */
+        StartOpenVPN(c);
 }
 
 void
